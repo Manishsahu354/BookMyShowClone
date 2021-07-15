28d67393d6bb29da439b041a3eb65ced363483a6
Manish Kumar <55155943+Manishsahu354@users.noreply.github.com>
2021-07-15 18:18:59 +0530

    Delete BookMyShow/app/build directory

--- BookMyShow/app/build/tmp/kapt3/stubs/debug/com/chaithanya/bookmyshow/ui/adapter/itemclicklistener/HomeFeaturesItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.chaithanya.bookmyshow.ui.adapter.itemclicklistener;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\b\n\u0000\bf\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lcom/chaithanya/bookmyshow/ui/adapter/itemclicklistener/HomeFeaturesItemClickListener;", "", "onFeaturesItemClicked", "", "position", "", "app_debug"})
-public abstract interface HomeFeaturesItemClickListener {
-    
-    public abstract void onFeaturesItemClicked(int position);
-}
